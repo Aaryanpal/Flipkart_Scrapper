cf734da9a97c838533abd29b2ec90cf9dcae9e4f
Aryan Pal <aryan.pal0ppq@gmail.com>
2022-12-29 18:54:52 +0530

    Added Multiple Page Scrapping and CSV

--- scrapper.rb
+++ scrapper.rb
@@ -1,5 +1,5 @@
 require 'open-uri'
-require 'nokogiri'
+require 'csv'
 require 'byebug'
 class Scrapper
 
@@ -10,12 +10,111 @@ class Scrapper
     def action
         request = open(@url)
         resp = request.read
-        # response = Nokogiri::HTML(URI.open(@url))
-        byebug
         identifier = '_4rR01T'
-        sol = resp.scan(identifier)
-        puts "Total Product: #{sol.count}"
+
+        found_indexes = []
+        current_index = 0
+=begin
+For Finding The indexes of Each similar Div That Hold The Product Name
+=end
+        while current_index!= nil
+            current_index = resp[(current_index + 1)..-1].index(identifier)
+            if current_index!=nil
+                current_index=current_index+1
+                if found_indexes.any?
+                    current_index = found_indexes.last + current_index
+                end
+                found_indexes << current_index
+            end
+        end
+=begin
+For Pushing The Name Of the product in an array
+=end
+        phones = []
+        count = 0
+        while(count< found_indexes.count)
+               res =  resp[ (found_indexes[count] + identifier.length + 2)..(found_indexes[count] + 37 )]
+               
+               if res!=nil
+                phones << res
+            end
+            count = count + 1; 
+        end
+
+=begin
+For Getting THe Result from Other Pages
+=end
+        page = 2
+        next_page_url = @url+"&page=#{page}"
+        req2 = open(next_page_url)
+        resp2 = req2.read
+
+        current_index = 0
+        while current_index!= nil
+            current_index = resp[(current_index + 1)..-1].index(identifier)
+            if current_index!=nil
+                current_index=current_index+1
+                if found_indexes.any?
+                    current_index = found_indexes.last + current_index
+                end
+                found_indexes << current_index
+            end
+        end
+
+        count = 0
+        while(count< found_indexes.count)
+               res =  resp[ (found_indexes[count] + identifier.length + 2)..(found_indexes[count] + 37 )]
+               
+               if res!=nil
+                phones << res
+            end
+            count = count + 1; 
+        end
+
+        page = 3
+        next_page_url = @url+"&page=#{page}"
+        req2 = open(next_page_url)
+        resp2 = req2.read
+
+        current_index = 0
+        while current_index!= nil
+            current_index = resp[(current_index + 1)..-1].index(identifier)
+            if current_index!=nil
+                current_index=current_index+1
+                if found_indexes.any?
+                    current_index = found_indexes.last + current_index
+                end
+                found_indexes << current_index
+            end
+        end
+
+        count = 0
+        while(count< found_indexes.count)
+               res =  resp[ (found_indexes[count] + identifier.length + 2)..(found_indexes[count] + 37 )]
+               
+               if res!=nil
+                phones << res
+            end
+            count = count + 1; 
+        end
+
+=begin
+For Writing The CSV REsult
+=end
+        CSV.open('result.csv','wb') do |csv|
+            csv << ["S.no","Phone "]
+           phones.each_with_index do |row,index|
+            csv << [index]
+            csv << [row]
+            
+        end
     end
+    byebug
+        puts "Total Product: #{phones.count}"
+    end
+    # def find_indexes
+        
+    # end
 end
 
 url = 'https://www.flipkart.com/search?q=smart%20phones&otracker=search&otracker1=search&mark
